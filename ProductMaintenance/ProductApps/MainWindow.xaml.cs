b64e6a9b85a8b7ac528af1eceea8737937d84c56
Jordan wallace <jordan.WALLACE3@student.tafesa.edu.au>
2026-09-07 12:31:13 +0930

    Commit the branch feat_wrap1.1

--- ProductMaintenance/ProductApps/MainWindow.xaml.cs
+++ ProductMaintenance/ProductApps/MainWindow.xaml.cs
@@ -34,8 +34,10 @@ namespace ProductApps
                 cProduct = new Product(Convert.ToDecimal(priceTextBox.Text), Convert.ToInt16(quantityTextBox.Text));
                 cProduct.calTotalPayment();
                 decimal totalCharge = cProduct.TotalPayment + 25;
+                decimal wrapCharge = totalCharge + 5;
                 totalPaymentTextBlock.Text = Convert.ToString(cProduct.TotalPayment);
                 totalChargeTextBox.Text = Convert.ToString(totalCharge);
+                WrapChargeTextBox.Text = Convert.ToString(wrapCharge);
             }
             catch (FormatException)
             {
